1ade3fc2a934ad1c5c9ac253cd28faa0e518bc2b
Edson Júnior <ebf.junin@gmail.com>
2018-04-28 15:46:41 -0300

    Changed directory structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './src/configureStore';
-import App from './App';
+import App from './src/App';
 
 const store = configureStore();
 
